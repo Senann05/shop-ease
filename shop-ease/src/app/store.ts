6b3e3286e30ef0977000092a065b6d8aa51b1cbb
Senann05 <senaneminli000@gmail.com>
2025-04-16 18:22:13 +0400

    cart updates

--- shop-ease/src/app/store.ts
+++ shop-ease/src/app/store.ts
@@ -1,16 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
+import cartReducer from '../features/cart/cartSlice'
 import { productApi } from '../features/products/productApi.ts'
 
 
 export const store = configureStore({
     reducer:{
         [productApi.reducerPath]: productApi.reducer,
+        cart: cartReducer
     },
     middleware: (getDefaultMiddleware)=>
         getDefaultMiddleware().concat(productApi.middleware)
 })
 
-setupListeners(store.dispatch);
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
